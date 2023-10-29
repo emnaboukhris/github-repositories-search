b2d10a1951d0d3828d6a2b4ddc62e9c51eb311a7
emnaboukhris <emna.boukhris@insat.ucar.tn>
2023-10-29 01:08:05 +0100

    handle list pagination

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,33 +1,43 @@
-import { PaginatedList } from 'react-paginated-list';
 import { Repo } from '../models/models';
 import RepoItem from './RepoItem';
 import { Divider } from '@mui/material';
+import { useState } from 'react';
+import ReactPaginate from 'react-paginate';
 
-// Props for the RepoList component
-export interface RepoListProps {
+interface RepoListProps {
   repositories: Repo[];
 }
 
 const RepoList: React.FC<RepoListProps> = ({ repositories }) => {
+  const itemsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const handlePageChange = (selectedPage: number) => {
+    setCurrentPage(selectedPage);
+  };
+
+  const offset = currentPage * itemsPerPage;
+  const paginatedRepos = repositories.slice(offset, offset + itemsPerPage);
+
   return (
     <div>
-      {/* Paginated List Component */}
-      <PaginatedList
-        list={repositories}
-        itemsPerPage={10}
-        renderList={(repos) => (
-          <>
-            {/* Mapping and Rendering Repository Items */}
-            {repos.map((repo, id) => {
-              return (
-                <>
-                  <RepoItem repo={repo}></RepoItem>
-                  <Divider />
-                </>
-              );
-            })}
-          </>
-        )}
+      {/* Mapping and Rendering Repository Items */}
+      {paginatedRepos.map((repo, id) => (
+        <div key={id}>
+          <RepoItem repo={repo} />
+          <Divider />
+        </div>
+      ))}
+
+      {/* Pagination Component */}
+      <ReactPaginate
+        pageCount={Math.ceil(repositories.length / itemsPerPage)}
+        pageRangeDisplayed={5}
+        marginPagesDisplayed={2}
+        onPageChange={({ selected }) => handlePageChange(selected)}
+        containerClassName="pagination"
+        disabledClassName={'pagination__link--disabled'}
+        activeClassName={'pagination__link--active'}
       />
     </div>
   );
